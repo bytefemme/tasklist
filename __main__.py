2d999cc3d096df6df56535e4213baf118ceeb717
bytefemme <nathaliabonillalozada@gmail.com>
2023-07-16 21:46:38 -0500

    feature(interface): add options for user to pick

--- __main__.py
+++ __main__.py
@@ -1,1 +1,17 @@
 print("¡Bienvenido a administrador de tareas! Esta aplicación te permitirá organizar y gestionar tus tareas de manera sencilla y eficiente. Podrás agregar nuevas tareas, eliminar las que ya no necesites y marcar como completadas aquellas que hayas finalizado")
+print("")
+selected_option = input("tasklist>>> ")
+
+if selected_option == "list":
+    print("List")
+elif selected_option == "create":
+    print("Create")
+elif selected_option == "delete":
+    print("Delete")
+elif selected_option == "complete":
+    print("Complete")
+elif selected_option == "quit":
+    print("Quit")
+else:
+    print("Help")
+    
