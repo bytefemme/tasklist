2916c6729d2469554199c72749c7f4536c77bf43
bytefemme <nathaliabonillalozada@gmail.com>
2023-07-18 19:14:44 -0500

    feature(list): add list feature and display mock tasks

--- __main__.py
+++ __main__.py
@@ -1,10 +1,22 @@
 print("¡Bienvenido a administrador de tareas! Esta aplicación te permitirá organizar y gestionar tus tareas de manera sencilla y eficiente. Podrás agregar nuevas tareas, eliminar las que ya no necesites y marcar como completadas aquellas que hayas finalizado")
 print("")
+
 selected_option = ""
 while selected_option != "quit":
     selected_option = input("tasklist>>> ")
     if selected_option == "list":
-        print("List")
+        task1 ={"id":1,"name":"nath","description":"sensual","date":"today","isComplete":False}
+        task2 ={"id":2,"name":"mike","description":"sexy","date":"today","isComplete":False}
+        task3 ={"id":3,"name":"jose","description":"handsome","date":"today","isComplete":False}
+        tasks = [task1, task2, task3]
+        for task in tasks:
+            print("id: " + str(task["id"]))
+            print("name: " + task["name"])
+            print("description: " + task["description"])
+            print("date: " + task["date"])
+            print("isComplete: " + str(task["isComplete"]))
+            print()
+
     elif selected_option == "add":
         print("Add")
     elif selected_option == "delete":
